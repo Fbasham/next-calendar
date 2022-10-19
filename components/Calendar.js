3ef7a1a73483d3da4452682055ef965c6e5fa79e
Frank Basham <frankabasham@gmail.com>
2022-10-18 20:16:01 -0400

    cleanup

--- components/Calendar.js
+++ components/Calendar.js
@@ -2,12 +2,17 @@ import { dateToMonthArray } from "../lib/calendar";
 
 function Calendar() {
   let dateArr = dateToMonthArray(new Date());
-  console.log(dateArr);
   return (
-    <div className="grid grid-cols-7">
+    <div className="grid grid-cols-7 h-screen text-sm">
       {dateArr.map((day, i) => (
-        <div key={i} className={`text-sm h-20 ${day ? "border" : ""}`}>
-          {day}
+        <div
+          key={i}
+          className={`flex flex-col p-[2px] ${
+            day ? "border border-indigo-100" : ""
+          }`}
+        >
+          <div className="text-end text-indigo-700 font-bold">{day}</div>
+          {day && <div className="h-full text-center">todo</div>}
         </div>
       ))}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Calendar from "../components/Calendar";
 
 export default function Index() {
   return (
-    <main className="max-w-4xl mx-auto">
+    <main className="max-w-4xl mx-auto px-5 pt-10">
       <Calendar />
     </main>
   );
