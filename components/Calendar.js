9e881ad3cc23faefb4dcdeff1c3dae49ed352f3d
Frank Basham <frankabasham@gmail.com>
2022-10-20 14:25:11 -0400

    more restructuring

--- components/Calendar.js
+++ components/Calendar.js
@@ -21,13 +21,13 @@ function Calendar({ events }) {
     <>
       <div className="flex gap-1 text-sm items-baseline text-indigo-900 mb-2">
         <button
-          className="border border-indigo-500 hover:bg-indigo-200 rounded px-1"
+          className="border border-indigo-500 hover:bg-indigo-800 hover:text-white rounded px-1"
           onClick={() => handleChangeMonth(-1)}
         >
           PREV
         </button>
         <button
-          className="border border-indigo-500 hover:bg-indigo-200 rounded px-1 mr-2"
+          className="border border-indigo-500 hover:bg-indigo-800 hover:text-white rounded px-1 mr-2"
           onClick={() => handleChangeMonth(1)}
         >
           NEXT
@@ -37,7 +37,7 @@ function Calendar({ events }) {
         </span>
         <button
           value={0}
-          className={`border border-indigo-500 hover:bg-indigo-200 rounded px-1 ml-auto ${
+          className={`border border-indigo-500 hover:bg-indigo-800 hover:text-white rounded px-1 ml-auto ${
             view == 0 ? "bg-indigo-700 text-white" : ""
           }`}
           onClick={handleViewClick}
@@ -46,7 +46,7 @@ function Calendar({ events }) {
         </button>
         <button
           value={1}
-          className={`border border-indigo-500 hover:bg-indigo-200 rounded px-1 ${
+          className={`border border-indigo-500 hover:bg-indigo-800 hover:text-white rounded px-1 ${
             view == 1 ? "bg-indigo-700 text-white" : ""
           }`}
           onClick={handleViewClick}
@@ -54,7 +54,11 @@ function Calendar({ events }) {
           LIST
         </button>
       </div>
-      <div className="grid grid-cols-7 border-t border-l font-bold text-sm text-indigo-900 bg-indigo-50">
+      <div
+        className={`grid grid-cols-7 border-t border-l font-bold text-sm text-indigo-900 bg-indigo-50 ${
+          view == 1 ? "hidden" : ""
+        }`}
+      >
         <div className="py-1 pl-2 border-r border-b">SUN</div>
         <div className="py-1 pl-2 border-r border-b">MON</div>
         <div className="py-1 pl-2 border-r border-b">TUE</div>
@@ -63,26 +67,52 @@ function Calendar({ events }) {
         <div className="py-1 pl-2 border-r border-b">FRI</div>
         <div className="py-1 pl-2 border-r">SAT</div>
       </div>
-      <div className="grid grid-cols-7 h-screen text-sm">
+      <div
+        className={`${
+          view == 0 ? "grid grid-cols-7 h-screen text-sm" : "block"
+        }`}
+      >
         {dateArr.map((day, i) => (
           <div
             key={i}
-            className={`flex flex-col p-[2px] ${
-              day
-                ? "border border-indigo-100 hover:bg-indigo-100 duration-100"
+            className={
+              view == 0
+                ? `flex flex-col p-[2px] ${
+                    day
+                      ? "border border-indigo-100 hover:bg-indigo-100 duration-100"
+                      : ""
+                  }`
+                : day
+                ? "border min-h-[6rem]"
                 : ""
-            }`}
+            }
           >
-            <div className="text-end text-indigo-700 font-bold">
+            <div
+              className={
+                day
+                  ? view == 0
+                    ? "text-end text-indigo-700 font-bold"
+                    : "bg-indigo-500 text-white font-bold px-2 h-8 flex items-center justify-center"
+                  : ""
+              }
+            >
               {day?.getDate()}
             </div>
             {day && (
-              <div className="grow basis-1 max-h-10 text-center overflow-hidden">
-                {
-                  events.find(
-                    (e) => new Date(e.date).getTime() === day.getTime()
-                  )?.event
+              <div
+                className={
+                  view == 0
+                    ? "grow basis-1 max-h-10 text-center overflow-hidden"
+                    : "text-center min-h-full py-1"
                 }
+              >
+                {events
+                  .filter((e) => new Date(e.date).getTime() === day.getTime())
+                  .map((e) => (
+                    <ul>
+                      <li>{e.event}</li>
+                    </ul>
+                  ))}
               </div>
             )}
           </div>
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,10 @@ export default function Index({ events }) {
 export async function getStaticProps() {
   let events = [
     { date: new Date(2022, 9, 1), event: "event 1" },
-    { date: new Date(2022, 9, 2), event: "event 2" },
+    { date: new Date(2022, 9, 2), event: "event 2.0" },
+    { date: new Date(2022, 9, 2), event: "event 2.1" },
+    { date: new Date(2022, 9, 2), event: "event 2.2" },
+    { date: new Date(2022, 9, 2), event: "event 2.3" },
     {
       date: new Date(2022, 9, 17),
       event: "event with long text to test wrapping blah blah blah blah blah ",
